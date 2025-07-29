47885dfb8e5fc2d81efe5c191ba06ec7e6da5203
Serban Amariei-Hondrea <amarieiserban2@gmail.com>
2025-07-29 18:20:29 +0300

    Create vectorInt.h

---
+++ problema-1.3/vectorInt.h
@@ -0,0 +1,16 @@
+#ifndef F_H_
+#define F_H_
+
+#define DIMMAX 31
+
+void citireVector(int a[], int n);
+void afis(int a[], int n);
+int pscalar(int a[], int b[], int n);
+int medieA(int a[], int n);
+int suma(int a[], int n);
+int maxi(int a[], int n);
+int mini(int a[], int n);
+int mediaG(int a[], int n);
+
+
+#endif
