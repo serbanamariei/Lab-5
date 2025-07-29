d41ef865af9fb0fdc24083d5634d774177b8bd72
Serban Amariei-Hondrea <amarieiserban2@gmail.com>
2025-07-29 18:25:38 +0300

    Create vectorReal.h

---
+++ problema-2.2/vectorReal.h
@@ -0,0 +1,17 @@
+#ifndef VECTORREAL_H_
+#define VECTORREAL_H_
+
+#define DIMMAX 20
+
+void citireVector(int a[], int n);
+void afis(int a[], int n);
+int medie(int a[], int n);
+double abatere(int a[], int n);
+int nrElemNU(int a[], int n);
+int yvector(int x[], int n, int y[]);
+int sistEc(int coef[6], int sol[2]);
+void init(int a[], int n);
+int pscalar(int a[], int b[], int n);
+
+
+#endif
